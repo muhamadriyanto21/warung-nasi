e0d42e6936b9cadddfb0f0a268b7232eb4dd7e79
Muhamad Riyanto <98597500+muhamadriyanto21@users.noreply.github.com>
2023-12-25 11:51:49 +0700

    add modal box

--- js/script.js
+++ js/script.js
@@ -6,8 +6,10 @@ const searchForm = document.querySelector(".search-form");
 const searchBox = document.querySelector("#search-box");
 const searchButton = document.querySelector("#search-button");
 
-hamburgerMenu.addEventListener("click", () => {
+hamburgerMenu.addEventListener("click", (e) => {
   navbarNav.classList.toggle("active");
+
+  e.preventDefault();
 });
 
 // if i click in out elemens
@@ -43,4 +45,42 @@ document.addEventListener("click", function (e) {
   if (!searchForm.contains(e.target) && !searchButton.contains(e.target)) {
     searchForm.classList.remove("active");
   }
+
+  if (
+    !searchForm.contains(e.target) &&
+    !shoppingCartButton.contains(e.target)
+  ) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+const shoppingCartButton = document.querySelector("#shopping-cart-button");
+const shoppingCart = document.querySelector("#shopping-cart");
+shoppingCartButton.addEventListener("click", (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+});
+
+// Modal Box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  });
+});
+
+// Click Button Close
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+// Klik Diluar Modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};

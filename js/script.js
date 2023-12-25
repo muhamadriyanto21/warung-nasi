84e8b831de478b0da9ec3794ad1dbccf6a716b09
Muhamad Riyanto <98597500+muhamadriyanto21@users.noreply.github.com>
2023-12-25 10:01:53 +0700

    menambah product card

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
 // Toggle class active for hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 const hamburgerMenu = document.querySelector("#hamburger-menu");
+
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+const searchButton = document.querySelector("#search-button");
+
 hamburgerMenu.addEventListener("click", () => {
   navbarNav.classList.toggle("active");
 });
 
-// if i click in out sidebar, it will remove nav
+// if i click in out elemens
 document.addEventListener("click", function (e) {
   if (!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+
+  // e.preventDefault();
 });
 
 // Toggle Class active for search form
-const searchForm = document.querySelector(".search-form");
-const searchBox = document.querySelector("#search-box");
-const searchButton = document.querySelector("#search-button");
+
 searchButton.addEventListener("click", function (e) {
   if (searchForm.classList.contains("active")) {
     // If search form is active, hide it
@@ -25,15 +34,13 @@ searchButton.addEventListener("click", function (e) {
     searchForm.classList.add("active");
     searchBox.focus();
   }
-
   e.preventDefault();
 });
 
-
 // Click Outside sidebar for remove nav
-const hamburger = document.querySelector('#hamburger-menu');
-document.addEventListener('click', function(e) {
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
   if (!searchForm.contains(e.target) && !searchButton.contains(e.target)) {
-    searchForm.classList.remove('active');
+    searchForm.classList.remove("active");
   }
 });
